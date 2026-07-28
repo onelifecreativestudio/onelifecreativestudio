406d13356190602ff6dd501a719492ca7b7f57a5
onelifecreativestudio <onelifecreativestudio@gmail.com>
2026-07-28 17:17:07 +1000

    Updates and creative-mentoring added
    Updated the site, still needs more work and images need correcting.

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ if (year) {
 }
 
 const revealItems = document.querySelectorAll('.reveal');
-
 if ('IntersectionObserver' in window) {
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
@@ -35,7 +34,6 @@ if ('IntersectionObserver' in window) {
       }
     });
   }, { threshold: 0.14 });
-
   revealItems.forEach((item) => observer.observe(item));
 } else {
   revealItems.forEach((item) => item.classList.add('is-visible'));
